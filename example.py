ab7f34a74f2909b472546c51091614ae7aae6de9
Nikolai Tschacher <nikolai.tschacher@informatik.hu-berlin.de>
2021-12-13 22:01:25 +0100

    update auto

--- example.py
+++ example.py
@@ -1,10 +1,8 @@
 import time
-import os
 import random
-import json
-import subprocess
 from mouse import humanMove
 from typing import humanTyping
+from .utils import *
 
 """
 You might have to adjust some coordinates. 
@@ -15,40 +13,6 @@ left screen.
 You can obtain the coordinates of your current mouse pointer with 
 the bash command on Linux `xdotool getmouselocation`
 """
-
-def getPageSource():
-  cmd = f'/usr/bin/node page_source.js'
-  ps = subprocess.check_output(cmd, shell=True)
-  return ps
-
-
-def getCoords(selector, randomize_within_bcr=True):
-  """
-  Example: `node coords.js "li:nth-of-type(3) a"`
-  """
-  cmd = f'/usr/bin/node coords.js "{selector}"'
-  coords = subprocess.check_output(cmd, shell=True)
-  parsed = json.loads(coords)
-
-  x = parsed['x']
-  y = parsed['y']
-
-  if randomize_within_bcr:
-    x += random.randrange(0, int(parsed['width']))
-    y += random.randrange(0, int(parsed['height']))
-
-  return x, y
-
-
-def startBrowser():
-  os.system('google-chrome --remote-debugging-port=9222 --start-maximized --disable-notifications &')
-  time.sleep(4)
-
-  # visit https://bot.incolumitas.com/#botChallenge
-  humanMove(168, 79)
-  time.sleep(random.uniform(0.5, 1.5))
-  humanTyping('bot.incolumitas.com\n', speed=(0.005, 0.008))
-  time.sleep(random.uniform(1.5, 2.5))
 
 
 def main():
---
+++ immobilienscout24.py
@@ -0,0 +1,9 @@
+"""
+
+this is an example how to scrape www.immobilienscout24.de with stealthy-scraping-tools
+
+www.immobilienscout24.de is protected by advanced bot protection
+
+advanced?
+"""
+
